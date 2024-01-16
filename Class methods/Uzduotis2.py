fcae1b451b50267eb1a9b95db2ca5681cbfc22fc
DziugasRR <dziugas.rub@gmail.com>
2024-01-16 23:42:15 +0200

    task 2 done

--- Class methods/Uzduotis2.py
+++ Class methods/Uzduotis2.py
@@ -9,9 +9,9 @@ stringreversion = StringReversion()
 result = stringreversion.reversed_string("Hello,")
 print(result)
 
-class StringReversion:
-    @staticmethod
-    def reversed_string(txt):
-        return txt[::-1]
-
-print(stringreversion.reversed_string("world!"))
+# class StringReversion:
+#     @staticmethod
+#     def reversed_string(txt):
+#         return txt[::-1]
+#
+# print(stringreversion.reversed_string("world!"))
